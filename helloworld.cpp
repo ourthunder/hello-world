be243f71bb661a9cf82ae9edb276bb19e3576153
ourthunder <sehumah@protonmail.com>
2020-05-03 01:19:01 -0500

    added python & c++ helloworld files

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello, World!" << endl;
+	cout << "\n-----------------------------------\nhow to run c++ code in the terminal\n-----------------------------------" << endl;
+	cout << "use g++ filename.cpp to compile the c++ code. It will produce an executable in the same directory called a.out which can be run by typing ./a.out" << endl;
+	
+	return 0;
+}
+
+/*
+how to run c++ code in the terminal
+
+use "g++ filename.cpp" to compile the c++ code. It will produce an executable in the same directory called "a.out"
+which can be run by typing "./a.out"
+*/
